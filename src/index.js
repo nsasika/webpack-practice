a805ea898cccc449f19fdfc5f6908302a3b53ba5
Nalin Padmasiri <nalinsasika9@gmail.com>
2023-04-09 01:41:09 +0800

    index file import renamed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App';
+import App from './components/App';
 import './style/main.less';
 
 ReactDOM.render(<App />, document.getElementById('root'));
